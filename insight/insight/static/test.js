56801f6805bf22f6883308d94777e249e6870133
ryongseong <xmssnsk2119@gmail.com>
2023-09-26 16:20:51 +0900

    layout.html에 static의 test.js 연결 및 result html 이동 수정 전

--- insight/insight/static/test.js
+++ insight/insight/static/test.js
@@ -1,6 +1,6 @@
 var num = 1;
 var q = {
-    1: {"title": "1번 문제","type": "EI", "A": "대답 E", "B": "대답 I"},
+    1: {"title": "1번 문제","type": "EI", "A": "dksldfkadflaknvslaknv", "B": "dlkanflkasnvlsanvlk"},
     2: {"title": "2번 문제","type": "EI", "A": "대답 E", "B": "대답 I"},
     3: {"title": "3번 문제","type": "EI", "A": "대답 E", "B": "대답 I"},
     4: {"title": "4번 문제","type": "SN", "A": "대답 S", "B": "대답 N"},
@@ -11,7 +11,7 @@ var q = {
     9: {"title": "9번 문제","type": "TF", "A": "대답 T", "B": "대답 F"},
     10: {"title": "10번 문제","type": "JP", "A": "대답 J", "B": "대답 P"},
     11: {"title": "11번 문제","type": "JP", "A": "대답 J", "B": "대답 P"},
-    12: {"title": "12번 문제","type": "JP", "A": "대답 J", "B": "대답 P"},
+    12: {"title": "12번 문제","type": "JP", "A": "대답 J", "B": "대답 P"}
 }
 
 function start() {
@@ -20,13 +20,13 @@ function start() {
     next();
 }
 
-$("#A").click(function() {
+$("#A").click(function () {
     var type = $("#type").val();
     var preValue = $("#" + type).val();
     $("#" + type).val(parseInt(preValue) + 1);
     next();
 });
-$("#B").click(function(){
+$("#B").click(function () {
     next();
 });
 
@@ -43,7 +43,7 @@ function next() {
         $(".q-num").html("Q" + num);
         $(".progress-txt").html(num + ' / 12');
         $(".progress-bar").attr('style', 'width: calc(100/12*' + num + '%)');
-        $("title").html(q[num]["title"]);
+        $("#title").html(q[num]["title"]);
         $("#type").val(q[num]["type"]);
         $("#A").html(q[num]["A"]);
         $("#B").html(q[num]["B"]);
